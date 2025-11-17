b1a4f22cdc41ea79b24ea1a4e17207fcd8769242
EstebanCanVaz <ecantovaz@gmail.com>
2025-11-17 00:06:47 -0600

    Agregar form, controlador y test de clientes

--- src/Vista/FrmMenuPrincipal.java
+++ src/Vista/FrmMenuPrincipal.java
@@ -157,6 +157,9 @@ public class FrmMenuPrincipal extends javax.swing.JFrame {
     }//GEN-LAST:event_jMenuItem2MouseClicked
 
     private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+        Vista.FrmClientes objetoFormularioCliente = new Vista.FrmClientes();
+        jDesktopPaneFormularios.add(objetoFormularioCliente);
+        objetoFormularioCliente.setVisible(true);
     }//GEN-LAST:event_jMenuItem1ActionPerformed
 
     private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
